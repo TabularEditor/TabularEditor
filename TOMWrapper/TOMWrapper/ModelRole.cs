b7b0e904ac067108cff3c54250c030ce314a8fe6
Daniel Otykier <do@kapacity.dk>
2017-01-16 13:28:14 +0100

    Support for metadatapermissions on roles

--- TOMWrapper/TOMWrapper/ModelRole.cs
+++ TOMWrapper/TOMWrapper/ModelRole.cs
@@ -4,18 +4,23 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TabularEditor.PropertyGridUI;
 using TOM = Microsoft.AnalysisServices.Tabular;
 
 namespace TabularEditor.TOMWrapper
 {
-    public partial class ModelRole
+    public partial class ModelRole: IDynamicPropertyObject
     {
         [Browsable(true), DisplayName("Row Level Filters"), Category("Security")]
         public RoleRLSIndexer RowLevelSecurity { get; private set; }
-        
+
+        [Browsable(true), DisplayName("Metadata Permissions"), Category("Security")]
+        public RolePermissionIndexer MetadataPermission { get; private set; }
+
         public void InitRLSIndexer()
         {
             RowLevelSecurity = new RoleRLSIndexer(this);
+            MetadataPermission = new RolePermissionIndexer(this);
         }
 
         public override TabularNamedObject Clone(string newName, bool includeTranslations)
@@ -58,5 +63,17 @@ namespace TabularEditor.TOMWrapper
             base.Delete();
         }
 
+        public bool Browsable(string propertyName)
+        {
+            switch (propertyName) {
+                case "MetadataPermission": return Model.Database.CompatibilityLevel >= 1400;
+                default:  return true;
+            }
+        }
+
+        public bool Editable(string propertyName)
+        {
+            return true;
+        }
     }
 }
