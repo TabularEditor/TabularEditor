c361938909fbcc47a88997b2f3fb488e1fd1342a
Daniel Otykier <do@kapacity.dk>
2017-11-16 11:52:05 +0100

    ScriptMethod attribute to decorate global static methods that should be invokable without specfying namespace/class name in a script.

---
+++ TabularEditor/Scripting/ScriptMethodAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabularEditor.Scripting
+{
+    /// <summary>
+    /// Public static methods that are decorated with this attribute, will be exposed to
+    /// the scripting engine as top-level methods, so that the end-user does not need to
+    /// specify the class name to invoke the method.
+    /// </summary>
+    public class ScriptMethodAttribute: Attribute
+    {
+    }
+}
