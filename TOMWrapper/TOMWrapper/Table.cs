ae501cc15452e6087bff7a1649ace20ba3b678b8
Daniel Otykier <otykier@gmail.com>
2018-11-08 20:40:24 +0100

    Fix formula fixup bug when doing batch renames

--- TOMWrapper/TOMWrapper/Table.cs
+++ TOMWrapper/TOMWrapper/Table.cs
@@ -396,8 +396,7 @@ namespace TabularEditor.TOMWrapper
                     // Fixup is not performed during an undo operation. We rely on the undo stack to fixup the expressions
                     // affected by the name change (the undo stack should contain the expression changes that were made
                     // when the name was initially changed).
-                    if (!Handler.UndoManager.UndoInProgress) FormulaFixup.DoFixup(this);
-                    FormulaFixup.BuildDependencyTree(this);
+                    if (!Handler.UndoManager.UndoInProgress) FormulaFixup.DoFixup(this, true);
                     Handler.EndUpdate();
                 }
 
