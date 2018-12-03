95e4506f52a9f3f7181a71ca671c5c3b7831e65d
Daniel Otykier <otykier@gmail.com>
2018-12-03 22:36:34 +0100

    Fix issue #212

--- TabularEditor/Scripting/PropertySerializer.cs
+++ TabularEditor/Scripting/PropertySerializer.cs
@@ -58,6 +58,7 @@ namespace TabularEditor.Scripting
                     case ObjectType.Measure:
                     case ObjectType.KPI:
                     case ObjectType.Relationship:
+                    case ObjectType.Perspective:
                         break;
                     default:
                         continue;
@@ -134,15 +135,24 @@ namespace TabularEditor.Scripting
                 if(partFraction == null)
                 {
                     if (p.StartsWith("["))
-                        partFraction = p.Substring(1);
+                    {
+                        if (p.EndsWith("]"))
+                        {
+                            partFraction = p.Substring(1, p.Length - 2);
+                            partsFixed.Add(partFraction.ToLower());
+                            partFraction = null;
+                        }
+                        else
+                            partFraction = p.Substring(1);
+                    }
                     else
-                        partsFixed.Add(p);
+                        partsFixed.Add(p.ToLower());
                 } else
                 {
                     if (p.EndsWith("]"))
                     {
                         partFraction += "." + p.Substring(0, p.Length - 1);
-                        partsFixed.Add(partFraction);
+                        partsFixed.Add(partFraction.ToLower());
                         partFraction = null;
                     }
                     else
@@ -152,30 +162,43 @@ namespace TabularEditor.Scripting
             parts = partsFixed.ToArray();
 
             var model = UI.UIController.Current.Handler?.Model;
-            if (model == null) return null;
-            TabularObject obj = model;
-            foreach (var part in parts)
+            if (model == null || parts.Length == 0) return null;
+            if (parts.Length == 1 && parts[0] == "model") return model;
+            switch(parts[0])
             {
-                if (part == "Model") continue;
-                if (obj is Model)
-                {
-                    obj = model.Tables[part];
-                    continue;
-                }
-                if (obj is Table)
-                {
-                    obj = (obj as Table).GetChildren().OfType<TabularNamedObject>().FirstOrDefault(c => c.Name == part);
-                    continue;
-                }
-                if (obj is Hierarchy)
-                {
-                    obj = (obj as Hierarchy).Levels.FirstOrDefault(l => l.Name == part);
-                    continue;
-                }
-                obj = null;
-                break;
+                case "model":
+                    var table = model.Tables.FirstOrDefault(x => x.Name.EqualsI(parts[1]));
+                    if (parts.Length == 2 || table == null) return table;
+                    var obj = table.GetChildren().OfType<TabularNamedObject>().FirstOrDefault(c => c.Name.EqualsI(parts[2]));
+                    if (parts.Length == 3 || obj == null) return obj;
+                    if (obj is Hierarchy && parts.Length == 4) return (obj as Hierarchy).Levels.FirstOrDefault(x => x.Name.EqualsI(parts[3]));
+                    if (obj is Measure && parts[3] == "kpi" && parts.Length == 4) return (obj as Measure).KPI;
+                    if (obj is Column && parts[3] == "variations" && parts.Length == 5) return (obj as Column).Variations.FirstOrDefault(x => x.Name.EqualsI(parts[4]));
+                    return null;
+
+                case "relationship": return model.Relationships.FirstOrDefault(x => x.Name.EqualsI(parts[1]));
+                case "datasource": return model.DataSources.FirstOrDefault(x => x.Name.EqualsI(parts[1]));
+                case "role": return model.Roles.FirstOrDefault(x => x.Name.EqualsI(parts[1]));
+                case "expression": return model.Expressions.FirstOrDefault(x => x.Name.EqualsI(parts[1]));
+                case "perspective": return model.Perspectives.FirstOrDefault(x => x.Name.EqualsI(parts[1]));
+                case "culture": return model.Cultures.FirstOrDefault(x => x.Name.EqualsI(parts[1]));
+                case "tablepartition":
+                    if (parts.Length != 3) return null;
+                    table = model.Tables.FirstOrDefault(x => x.Name.EqualsI(parts[1]));
+                    if (table == null) return null;
+                    return table.Partitions.FirstOrDefault(x => x.Name.EqualsI(parts[2]));
+
+                default:
+                    // "Reseller Sales.Sales Amount" is equivalent to "Model.Reseller Sales.Sales Amount":
+                    table = model.Tables.FirstOrDefault(x => x.Name.EqualsI(parts[0]));
+                    if (parts.Length == 1 || table == null) return null;
+                    obj = table.GetChildren().OfType<TabularNamedObject>().FirstOrDefault(c => c.Name.EqualsI(parts[1]));
+                    if (parts.Length == 2 || obj == null) return obj;
+                    if (obj is Hierarchy && parts.Length == 3) return (obj as Hierarchy).Levels.FirstOrDefault(x => x.Name.EqualsI(parts[2]));
+                    if (obj is Measure && parts[2] == "kpi" && parts.Length == 3) return (obj as Measure).KPI;
+                    if (obj is Column && parts[2] == "variations" && parts.Length == 4) return (obj as Column).Variations.FirstOrDefault(x => x.Name.EqualsI(parts[3]));
+                    return null;
             }
-            return obj;
         }
     }
 }
