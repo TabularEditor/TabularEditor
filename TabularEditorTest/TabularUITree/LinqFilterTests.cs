73d80a1e15611403c21b28078f9537f5e00ebcbd
Daniel Otykier <do@kapacity.dk>
2019-02-11 13:41:36 +0100

    Cleaned up TabularUITree code and added unit test

---
+++ TabularEditorTest/TabularUITree/LinqFilterTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Aga.Controls.Tree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TabularEditor.TOMWrapper;
+
+namespace TabularEditor
+{
+    [TestClass]
+    public class LinqFilterTests
+    {
+        [TestMethod]
+        public void BasicLinqFilterTest()
+        {
+            var handler = new TabularModelHandler("localhost", "AdventureWorks");
+            var uiTree = new TabularUITree(handler);
+
+            uiTree.FilterMode = FilterMode.Flat;
+
+            uiTree.Filter = null;
+            uiTree.Filter = ":ObjectType=\"Table\"";
+            var items = uiTree.GetChildrenInternal(TreePath.Empty).ToList();
+
+            Assert.AreEqual(14, items.Count);
+
+        }
+
+        [TestMethod]
+        public void NullSafetyTest()
+        {
+            var handler = new TabularModelHandler("localhost", "AdventureWorks");
+            var uiTree = new TabularUITree(handler);
+            handler.Model.Tables["Currency"].SetAnnotation("NotNull", "This is a test");
+
+            uiTree.FilterMode = FilterMode.Flat;
+
+            uiTree.Filter = null;
+            uiTree.Filter = ":GetAnnotation(\"NotNull\").Contains(\"test\")";
+            var items = uiTree.GetChildrenInternal(TreePath.Empty).ToList();
+
+            Assert.AreEqual(1, items.Count);
+        }
+
+        [TestMethod]
+        public void PerformanceTest()
+        {
+            var handler = new TabularModelHandler("localhost", "AdventureWorks");
+            var uiTree = new TabularUITree(handler);
+            handler.Model.Tables["Currency"].SetAnnotation("NotNull", "This is a test");
+
+            uiTree.FilterMode = FilterMode.Flat;
+
+            var items = new List<ITabularNamedObject>();
+
+            var sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                uiTree.Filter = null;
+                uiTree.Filter = ":GetAnnotation(\"NotNull\").Contains(\"test\")";
+                items.AddRange(uiTree.GetChildrenInternal(TreePath.Empty));
+            }
+            sw.Stop();
+
+            // Run 1: 3935
+            // Run 2: 4042
+
+            Assert.IsTrue(sw.ElapsedMilliseconds < 5000);
+            Assert.AreEqual(1000, items.Count);
+        }
+    }
+}
