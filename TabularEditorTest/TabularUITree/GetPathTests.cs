7a60993bebcbc89e39ed39b104b5ef20a98aef02
Daniel Otykier <do@kapacity.dk>
2019-06-20 13:53:51 +0200

    Fix bug where the "Attribute"-column doesn't show up upon creation of new calc group

--- TabularEditorTest/TabularUITree/GetPathTests.cs
+++ TabularEditorTest/TabularUITree/GetPathTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Linq.Dynamic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TabularEditor.TOMWrapper;
 
@@ -9,11 +10,13 @@ namespace TabularEditor
     public class GetPathTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestWithDisplayFolders()
         {
             var handler = new TabularModelHandler(1470);
             var tree = new TabularUITree(handler);
             var model = handler.Model;
+
+            tree.Options = LogicalTreeOptions.Default;
 
             var t1 = model.AddTable("Table 1");
             t1.AddDataColumn("Column 1");
@@ -23,16 +26,52 @@ namespace TabularEditor
             cg1.Field.Name = "Field";
             var ci1 = cg1.AddCalculationItem("Item 1");
 
-            tree.Options = LogicalTreeOptions.Default;
+            var cg1Path = tree.GetPath(cg1);
+            var ci1Path = tree.GetPath(ci1);
 
-            Assert.AreEqual(5, tree.GetPath(ci1).FullPath.Length);
+            Assert.AreEqual(5, ci1Path.FullPath.Length);
             Assert.IsTrue(
-                tree.GetPath(ci1).FullPath.SequenceEqual(
+                ci1Path.FullPath.SequenceEqual(
                     new object[] { model, model.Groups.Tables, cg1, cg1.Field, ci1 }));
+
+            Assert.AreEqual(1, tree.GetChildren(cg1Path).Count());
 
             cg1.Field.DisplayFolder = "Folder\\Subfolder";
 
             Assert.AreEqual(7, tree.GetPath(ci1).FullPath.Length);
+
+        }
+
+        [TestMethod]
+        public void TestWithoutDisplayFolders()
+        {
+            var handler = new TabularModelHandler(1470);
+            var tree = new TabularUITree(handler);
+            var model = handler.Model;
+
+            tree.Options = LogicalTreeOptions.Default & ~LogicalTreeOptions.DisplayFolders;
+
+            var t1 = model.AddTable("Table 1");
+            t1.AddDataColumn("Column 1");
+            t1.AddMeasure("Measure 1");
+
+            var cg1 = model.AddCalculationGroup("Calc Group 1");
+            cg1.Field.Name = "Field";
+            var ci1 = cg1.AddCalculationItem("Item 1");
+
+            var cg1Path = tree.GetPath(cg1);
+            var ci1Path = tree.GetPath(ci1);
+
+            Assert.AreEqual(5, ci1Path.FullPath.Length);
+            Assert.IsTrue(
+                ci1Path.FullPath.SequenceEqual(
+                    new object[] { model, model.Groups.Tables, cg1, cg1.Field, ci1 }));
+
+            Assert.AreEqual(1, tree.GetChildren(cg1Path).Count());
+
+            cg1.Field.DisplayFolder = "Folder\\Subfolder";
+
+            Assert.AreEqual(5, tree.GetPath(ci1).FullPath.Length);
 
         }
     }
