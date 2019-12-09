10ef2fae9272288fe6414a6502dae3b3633e181a
Daniel Otykier <do@kapacity.dk>
2019-12-09 09:30:35 +0100

    Let TextBox accept tabs/returns

--- TabularEditor/UI/Dialogs/Pages/ImportTableFromMetadataPage.Designer.cs
+++ TabularEditor/UI/Dialogs/Pages/ImportTableFromMetadataPage.Designer.cs
@@ -76,6 +76,7 @@
             // 
             // richTextBox1
             // 
+            this.richTextBox1.AcceptsTab = true;
             this.richTextBox1.DetectUrls = false;
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.richTextBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
