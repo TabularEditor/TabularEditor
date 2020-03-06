fd8e98ff9bb8a06f95db72f7e43c6bebd4be3046
Daniel Otykier <do@kapacity.dk>
2020-03-06 14:07:16 +0100

    Added unit tests

---
+++ TOMWrapperTest/RegressionTests/v2_9_2/DetailRowsDefinitionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TabularEditor.TOMWrapper;
+
+namespace TOMWrapperTest.RegressionTests.v2_9_2
+{
+    [TestClass]
+    public class DetailRowsDefinitionTests
+    {
+        [TestMethod]
+        public void DetailRowsDefinitionHack()
+        {
+            var handler = ObjectHandlingTests.CreateTestModel(compatibilityLevel: 1500);
+            var model = handler.Model;
+            var t1 = model.Tables["Test Table 1"];
+            t1.DefaultDetailRowsExpression = "abc";
+            t1.DefaultDetailRowsExpression = null;
+
+            var m1 = t1.Measures["Measure 1"];
+            m1.DetailRowsExpression = "123";
+            m1.DetailRowsExpression = null;
+        }
+    }
+}
