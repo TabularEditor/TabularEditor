1baf287d65522d876a68fed9606763be77bf55c2
Daniel Otykier <do@kapacity.dk>
2020-07-31 10:17:10 +0200

    Support for StructuredDataSource ConnectionDetails Address property collection

---
+++ TOMWrapper/PropertyGridUI/CollectionEditors/ConnectionAddressPropertyCollectionEditor.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AnalysisServices.Tabular;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TabularEditor.PropertyGridUI.CollectionEditors
+{
+    // TODO: Consolidate this class and the DataSourceOptionsPropertyCollectionEditor
+    internal class ConnectionAddressPropertyCollectionEditor : RefreshGridCollectionEditor
+    {
+        public ConnectionAddressPropertyCollectionEditor() : base(typeof(Collection<ConnectionAddressProperty>))
+        {
+
+        }
+        protected override object[] GetItems(object editValue)
+        {
+            if (editValue is object[] editValueResult)
+            {
+                return editValueResult;
+            }
+            else if (editValue is TOMWrapper.StructuredDataSource.AddressImpl adr)
+            {
+                this.adr = adr;
+                this.lastAssigned = adr.Keys.Select(k => new ConnectionAddressProperty(k, adr[k].ToString())).ToArray();
+                return lastAssigned;
+            }
+            else
+                throw new NotSupportedException();
+        }
+
+        internal ConnectionAddressProperty[] lastAssigned;
+        private TOMWrapper.StructuredDataSource.AddressImpl adr;
+
+        protected override object SetItems(object editValue, object[] value)
+        {
+            if (Cancelled) return editValue;
+            lastAssigned = value.Cast<ConnectionAddressProperty>().ToArray();
+            return value;
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            if (!Cancelled && lastAssigned != null)
+            {
+                // Add new items / remove non-existing items:
+                foreach (var adrProp in lastAssigned.Where(prop => prop.Value != null))
+                {
+                    adr[adrProp.Property] = adrProp.Value;
+                }
+                foreach (var adrKey in adr.Keys)
+                {
+                    if (!lastAssigned.Any(cp => cp.Property == adrKey))
+                        adr[adrKey] = null;
+                }
+            }
+
+            base.OnFormClosed(e);
+        }
+
+        protected override object CreateInstance(Type itemType)
+        {
+            return new ConnectionAddressProperty(this);
+        }
+    }
+
+    internal class ConnectionAddressProperty
+    {
+        private readonly ConnectionAddressPropertyCollectionEditor owner;
+        public ConnectionAddressProperty(ConnectionAddressPropertyCollectionEditor owner)
+        {
+            this.owner = owner;
+        }
+        public ConnectionAddressProperty(string property, string value)
+        {
+            this.property = property;
+            this.Value = value;
+        }
+
+        private string property;
+        [Category("Basic"), TypeConverter(typeof(ConnectionAddressPropertyConverter))]
+        public string Property
+        {
+            get => this.property;
+            set
+            {
+                if (owner.lastAssigned.Any(p => p.Property == value))
+                    throw new ArgumentException("The ConnectionAddress collection already contains this property");
+                this.property = value;
+            }
+        }
+        [Category("Basic")]
+        public string Value { get; set; }
+        public override string ToString()
+        {
+            return Property;
+        }
+    }
+
+
+    class ConnectionAddressPropertyConverter : StringConverter
+    {
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            var ConnectionAddressPropertyTypeInfo = typeof(Microsoft.AnalysisServices.Tabular.ConnectionAddressProperty);
+            var fieldInfos = ConnectionAddressPropertyTypeInfo.GetFields(BindingFlags.Public | BindingFlags.Static);
+
+            return new StandardValuesCollection(
+                fieldInfos.Where(fi => fi.IsLiteral && !fi.IsInitOnly).Select(fi => fi.GetRawConstantValue().ToString()).ToList()
+                );
+        }
+
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+    }
+}
--- TOMWrapper/TOMWrapper/StructuredDataSource.cs
+++ TOMWrapper/TOMWrapper/StructuredDataSource.cs
@@ -43,9 +43,10 @@ namespace TabularEditor.TOMWrapper
             if (MetadataObject.Options == null) MetadataObject.Options = new TOM.DataSourceOptions();
             this.Credential = new CredentialImpl(this);
             this.Options = new DataSourceOptionsImpl(this);
+            this.Address = new AddressImpl(this);
         }
 
-        [Category("Basic")]
+        [Category("Basic"),Description("Connection protocol"),IntelliSense("Connection protocol")]
         public string Protocol {
             get { return MetadataObject.ConnectionDetails.Protocol; }
             set { SetValue(Protocol, value, v => { MetadataObject.ConnectionDetails.Protocol = (string)v; }, "Protocol"); }
@@ -53,12 +54,18 @@ namespace TabularEditor.TOMWrapper
         private bool ShouldSerializeProtocol() { return false; }
 
         [Category("Options"), Editor(typeof(DataSourceOptionsPropertyCollectionEditor), typeof(UITypeEditor)), Description("Protocol-specific options used to connect the data source")]
+        [IntelliSense("Protocol-specific options used to connect the data source")]
         public DataSourceOptionsImpl Options { get; private set; }
 
         [Category("Credential"),Editor(typeof(CredentialPropertyCollectionEditor),typeof(UITypeEditor)), Description("Protocol-specific options used to authenticate the connection")]
+        [IntelliSense("Protocol-specific options used to authenticate the connection")]
         public CredentialImpl Credential { get; private set; }
-        
-        [Category("Credential")]
+
+        [Category("Connection Details"), Editor(typeof(ConnectionAddressPropertyCollectionEditor), typeof(UITypeEditor)), Description("Protocol-specific address options for this connection")]
+        [IntelliSense("Protocol-specific options used to authenticate the connection")]
+        public AddressImpl Address { get; private set; }
+
+        [Category("Credential"), Description("Username property or null if it's not set"), IntelliSense("Username property or null if it's not set")]
         public string Username
         {
             get { return MetadataObject.Credential.Username; }
@@ -66,7 +73,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeUsername() { return false; }
 
-        [Category("Credential"),PasswordPropertyText(true)]
+        [Category("Credential"), Description("Password property or null if it's not set"), IntelliSense("Password property or null if it's not set"), PasswordPropertyText(true)]
         public string Password
         {
             get { return MetadataObject.Credential.Password; }
@@ -74,7 +81,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializePassword() { return false; }
 
-        [Category("Credential")]
+        [Category("Credential"), Description("Privacy setting from PrivacyClass or null if it's not present in the Credential property. Missing value is equivalent to the value of PrivacyClass.None."), IntelliSense("Privacy setting from PrivacyClass or null if it's not present in the Credential property. Missing value is equivalent to the value of PrivacyClass.None.")]
         public string PrivacySetting
         {
             get { return MetadataObject.Credential.PrivacySetting; }
@@ -82,7 +89,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializePrivacySetting() { return false; }
 
-        [Category("Credential")]
+        [Category("Credential"), Description("Authentication kind, usually one of the AuthenticationKind constants"), IntelliSense("Authentication kind, usually one of the AuthenticationKind constants")]
         public string AuthenticationKind
         {
             get { return MetadataObject.Credential.AuthenticationKind; }
@@ -90,7 +97,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeAuthenticationKind() { return false; }
 
-        [Category("Credential")]
+        [Category("Credential"), Description("Whether connection must be encrypted"), IntelliSense("Whether connection must be encrypted")]
         public bool EncryptConnection
         {
             get { return MetadataObject.Credential.EncryptConnection; }
@@ -98,7 +105,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeEncryptConnection() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("Account (Connection Address proprety)"), IntelliSense("Account (Connection Address proprety)")]
         public string Account
         {
             get { return MetadataObject.ConnectionDetails.Address.Account; }
@@ -106,7 +113,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeAccount() { return false; }
 
-        [Category("Connection Details"),DisplayName("Connection String")]
+        [Category("Connection Details"), Description("Connection string (Connection Address property)"), IntelliSense("Connection string (Connection Address property)"), DisplayName("Connection String")]
         public string ConnectionString
         {
             get { return MetadataObject.ConnectionDetails.Address.ConnectionString; }
@@ -114,7 +121,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeConnectionString() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("Content type (Connection Address property)"), IntelliSense("Content type (Connection Address property)")]
         public string ContentType
         {
             get { return MetadataObject.ConnectionDetails.Address.ContentType; }
@@ -122,7 +129,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeContentType() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("Database name (Connection Address property)"), IntelliSense("Database name (Connection Address property)")]
         public string Database
         {
             get { return MetadataObject.ConnectionDetails.Address.Database; }
@@ -130,7 +137,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeDatabase() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("Domain (Connection Address property)"), IntelliSense("Domain (Connection Address property)")]
         public string Domain
         {
             get { return MetadataObject.ConnectionDetails.Address.Domain; }
@@ -138,7 +145,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeDomain() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("Email address (Connection Address property)"), IntelliSense("Email address (Connection Address property)")]
         public string EmailAddress
         {
             get { return MetadataObject.ConnectionDetails.Address.EmailAddress; }
@@ -146,7 +153,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeEmailAddress() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("Model name (Connection Address property)"), IntelliSense("Model name (Connection Address property)")]
         public new string Model
         {
             get { return MetadataObject.ConnectionDetails.Address.Model; }
@@ -154,7 +161,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeModel() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("Object (Connection Address property)"), IntelliSense("Object (Connection Address property)")]
         public string Object
         {
             get { return MetadataObject.ConnectionDetails.Address.Object; }
@@ -162,7 +169,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeObject() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("Path (Connection Address property)"), IntelliSense("Path (Connection Address property)")]
         public string Path
         {
             get { return MetadataObject.ConnectionDetails.Address.Path; }
@@ -170,7 +177,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializePath() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("Property (Connection Address property)"), IntelliSense("Property (Connection Address property)")]
         public string Property
         {
             get { return MetadataObject.ConnectionDetails.Address.Property; }
@@ -178,7 +185,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeProperty() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("Resource (Connection Address property)"), IntelliSense("Resource (Connection Address property)")]
         public string Resource
         {
             get { return MetadataObject.ConnectionDetails.Address.Resource; }
@@ -186,7 +193,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeResource() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("Schema name (Connection Address property)"), IntelliSense("Schema name (Connection Address property)")]
         public string Schema
         {
             get { return MetadataObject.ConnectionDetails.Address.Schema; }
@@ -194,7 +201,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeSchema() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("Server address (Connection Address property)"), IntelliSense("Server address (Connection Address property)")]
         public string Server
         {
             get { return MetadataObject.ConnectionDetails.Address.Server; }
@@ -202,7 +209,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeServer() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("Url (Connection Address property)"), IntelliSense("Url (Connection Address property)")]
         public string Url
         {
             get { return MetadataObject.ConnectionDetails.Address.Url; }
@@ -210,7 +217,7 @@ namespace TabularEditor.TOMWrapper
         }
         private bool ShouldSerializeUrl() { return false; }
 
-        [Category("Connection Details")]
+        [Category("Connection Details"), Description("View (Connection Address property)"), IntelliSense("View (Connection Address property)")]
         public string View
         {
             get { return MetadataObject.ConnectionDetails.Address.View; }
@@ -238,7 +245,7 @@ namespace TabularEditor.TOMWrapper
                 {
                     var json = TomCredential.ToString();
                     if (!string.IsNullOrEmpty(json))
-                        return JObject.Parse(TomCredential.ToString()).Properties().Select(p => p.Name);
+                        return JObject.Parse(json).Properties().Select(p => p.Name);
                     else return Enumerable.Empty<string>();
                 }
             }
@@ -282,7 +289,7 @@ namespace TabularEditor.TOMWrapper
                 {
                     var json = TomOptions.ToString();
                     if (!string.IsNullOrEmpty(json))
-                        return JObject.Parse(TomOptions.ToString()).Properties().Select(p => p.Name);
+                        return JObject.Parse(json).Properties().Select(p => p.Name);
                     else return Enumerable.Empty<string>();
                 }
             }
@@ -298,6 +305,50 @@ namespace TabularEditor.TOMWrapper
                     var oldValue = TomOptions[index];
                     TomOptions[index] = value;
                     dataSource.Handler.UndoManager.Add(new UndoDataSourceOptionsAction(dataSource, index, value as string, oldValue as string));
+                }
+            }
+
+
+            public string GetDisplayName(string key)
+            {
+                return key;
+            }
+        }
+
+        [TypeConverter(typeof(IndexerConverter))]
+        public class AddressImpl : IExpandableIndexer
+        {
+            private readonly StructuredDataSource dataSource;
+            private TOM.ConnectionAddress TomAddress => dataSource.MetadataObject.ConnectionDetails.Address;
+            public AddressImpl(StructuredDataSource dataSource)
+            {
+                this.dataSource = dataSource;
+            }
+
+            public string Summary => "(Click to edit)";
+
+            public IEnumerable<string> Keys
+            {
+                get
+                {
+                    var json = dataSource.MetadataObject.ConnectionDetails.ToString();
+                    if (!string.IsNullOrEmpty(json))
+                        return (JObject.Parse(json)["address"] as JObject)?.Properties().Select(p => p.Name) ?? Enumerable.Empty<string>();
+                    else return Enumerable.Empty<string>();
+                }
+            }
+
+            public bool EnableMultiLine => false;
+
+            public object this[string index]
+            {
+                get => TomAddress[index];
+                set
+                {
+                    if (TomAddress[index] == value) return;
+                    var oldValue = TomAddress[index];
+                    TomAddress[index] = value;
+                    dataSource.Handler.UndoManager.Add(new UndoConnectionAddressAction(dataSource, index, value as string, oldValue as string));
                 }
             }
 
