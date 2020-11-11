37c37d0d2c35960683c75ecabed3dc7dfd183bc9
Daniel Otykier <do@kapacity.dk>
2020-11-11 15:50:05 +0100

    Use HTTPS and redirect for DAX Formatter calls. Made DAXFormatter timeouts configurable and allows config of skipSpaceAfterFunctionName

--- TabularEditor/UIServices/DaxFormatter.cs
+++ TabularEditor/UIServices/DaxFormatter.cs
@@ -25,29 +25,48 @@ namespace TabularEditor.Dax
         public string message;
     }
 
-    public class DaxFormatterRequest
+    public class ServerDatabaseInfo
+    {
+        public string ServerName { get; set; } // SHA-256 hash of server name
+        public string ServerEdition { get; set; } // # Values: null, "Enterprise64", "Developer64", "Standard64"
+        public string ServerType { get; set; } // Values: null, "SSAS", "PBI Desktop", "SSDT Workspace", "Tabular Editor"
+        public string ServerMode { get; set; } // Values: null, "SharePoint", "Tabular"
+        public string ServerLocation { get; set; } // Values: null, "OnPremise", "Azure"
+        public string ServerVersion { get; set; } // Example: "14.0.800.192"
+        public string DatabaseName { get; set; } // SHA-256 hash of database name
+        public string DatabaseCompatibilityLevel { get; set; } // Values: 1200, 1400
+    }
+
+    public class DaxFormatterRequest : ServerDatabaseInfo
     {
         public string Dax { get; set; }
-        public int MaxLineLenght { get; set; } = 0;
+        public int? MaxLineLenght { get; set; }
+        public bool? SkipSpaceAfterFunctionName { get; set; }
         public char ListSeparator { get; set; }
         public char DecimalSeparator { get; set; }
         public string CallerApp { get; set; }
         public string CallerVersion { get; set; }
 
-        public string ServerName { get; set; }
-        public string ServerEdition { get; set; }
-        public string ServerType { get; set; }
-        public string ServerMode { get; set; }
-        public string ServerLocation { get; set; }
-        public string ServerVersion { get; set; }
-        public string DatabaseName { get; set; }
-        public string DatabaseCompatibilityLevel { get; set; }
+        public DaxFormatterRequest()
+        {
+            this.ListSeparator = ',';
+            this.DecimalSeparator = '.';
 
-        public DaxFormatterRequest(bool useSemicolonsAsSeparators, bool shortFormat)
+            // Save caller app and version
+            var assemblyName = System.Reflection.Assembly.GetEntryAssembly().GetName();
+            this.CallerApp = assemblyName.Name;
+            this.CallerVersion = assemblyName.Version.ToString();
+
+            var telemetry = ModelTelemetry.Collect();
+            if (telemetry != null) PopulateFromTelemetry(telemetry);
+        }
+
+        public DaxFormatterRequest(bool useSemicolonsAsSeparators, bool shortFormat, bool skipSpaceAfterFunctionName)
         {
             this.ListSeparator = useSemicolonsAsSeparators ? ';' : ',';
             this.DecimalSeparator = useSemicolonsAsSeparators ? ',' : '.';
             this.MaxLineLenght = shortFormat ? 1 : 0;
+            this.SkipSpaceAfterFunctionName = skipSpaceAfterFunctionName;
 
             // Save caller app and version
             var assemblyName = System.Reflection.Assembly.GetEntryAssembly().GetName();
@@ -81,12 +100,20 @@ namespace TabularEditor.Dax
 
     public class DaxFormatterProxy
     {
-        public const string DaxTextFormatUri = "http://daxtest02.azurewebsites.net/api/daxformatter/DaxFormat";
-        public const int DaxFormatterRequestTimeout = 10;
-
-        public static DaxFormatterResult FormatDax(string query, bool useSemicolonsAsSeparators, bool shortFormat)
+        static DaxFormatterProxy()
         {
-            string output = CallDaxFormatter(DaxTextFormatUri, query, useSemicolonsAsSeparators, shortFormat);
+            // force the use of TLS 1.2
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
+
+        public const string DaxTextFormatUri = "https://www.daxformatter.com/api/daxformatter/DaxTextFormat";
+
+        private static string redirectUrl;  // cache the redirected URL
+        private static string redirectHost;
+
+        public static DaxFormatterResult FormatDax(string query, bool useSemicolonsAsSeparators, bool shortFormat, bool skipSpaceAfterFunctionName)
+        {
+            string output = CallDaxFormatter(DaxTextFormatUri, query, useSemicolonsAsSeparators, shortFormat, skipSpaceAfterFunctionName);
             var res2 = new DaxFormatterResult();
             if (output.StartsWith("{"))
             {
@@ -98,13 +125,18 @@ namespace TabularEditor.Dax
             return res2;
         }
 
-        private static string CallDaxFormatter(string uri, string query, bool useSemicolonsAsSeparators, bool shortFormat)
+        private static string CallDaxFormatter(string uri, string query, bool useSemicolonsAsSeparators, bool shortFormat, bool skipSpaceAfterFunctionName)
         {
             try
             {
+                PrimeConnection(uri);
+                var originalUri = new Uri(uri);
+                var actualUri = new UriBuilder(originalUri.Scheme, redirectHost, originalUri.Port, originalUri.PathAndQuery).ToString();
 
-                DaxFormatterRequest req = new DaxFormatterRequest(useSemicolonsAsSeparators, shortFormat);
-                req.Dax = query;
+                var req = new DaxFormatterRequest(useSemicolonsAsSeparators, shortFormat, skipSpaceAfterFunctionName)
+                {
+                    Dax = query
+                };
 
                 var data = JsonConvert.SerializeObject(req, Formatting.Indented);
 
@@ -115,11 +147,10 @@ namespace TabularEditor.Dax
                 // see: http://stackoverflow.com/questions/566437/http-post-returns-the-error-417-expectation-failed-c
                 //System.Net.ServicePointManager.Expect100Continue = false;
 
-                var wr = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(uri);
-                wr.Proxy = ProxyCache.GetProxy(uri);
+                var wr = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(actualUri);
+                wr.Proxy = ProxyCache.GetProxy(actualUri);
 
-                wr.Timeout = DaxFormatterRequestTimeout * 1000;
-                wr.ContentType = "application/json";
+                wr.Timeout = Preferences.Current.DaxFormatterRequestTimeout;
                 wr.Method = "POST";
                 wr.Accept = "application/json, text/javascript, */*; q=0.01";
                 wr.Headers.Add("Accept-Encoding", "gzip,deflate");
@@ -151,6 +182,37 @@ namespace TabularEditor.Dax
             }
             finally
             {
+            }
+        }
+
+        private static void PrimeConnection(string uri)
+        {
+            if (redirectHost == null)
+            {
+                // www.daxformatter.com redirects request to another site.  HttpWebRequest does redirect with GET.  It fails, since the web service works only with POST
+                // The following 2 requests are doing manual POST re-direct
+                //var webRequestFactory = IoC.Get<WebRequestFactory>();
+                var redirectRequest = (HttpWebRequest)WebRequest.Create(uri);
+                redirectRequest.Proxy = ProxyCache.GetProxy(uri);
+
+                redirectRequest.AllowAutoRedirect = false;
+                redirectRequest.Timeout = Preferences.Current.DaxFormatterRequestTimeout;
+                try
+                {
+                    using (var netResponse = redirectRequest.GetResponse())
+                    {
+                        var redirectResponse = (HttpWebResponse)netResponse;
+                        redirectUrl = redirectResponse.Headers["Location"];
+                        var redirectUri = new Uri(redirectUrl);
+
+                        // set the shared redirectHost variable
+                        redirectHost = redirectUri.Host;
+                    }
+                }
+                catch
+                {
+                    
+                }
             }
         }
     }
