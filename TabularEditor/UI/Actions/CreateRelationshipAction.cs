3123cdf03fc5633e3ad84aa2df0e2f1178464744
Daniel Otykier <do@kapacity.dk>
2021-03-18 21:22:15 +0100

    Fix issue #805

--- TabularEditor/UI/Actions/CreateRelationshipAction.cs
+++ TabularEditor/UI/Actions/CreateRelationshipAction.cs
@@ -39,7 +39,7 @@ namespace TabularEditor.UI.Actions
                 if (col == null) return new Dictionary<string, object>();
                 var relatedTables = new HashSet<Table>(col.Table.RelatedTables);
 
-                var candidateCols = UI.Handler.Model.Tables
+                var candidateCols = col.Model.Tables
                     .Where(t => t != col.Table) // All tables except parent table of current column
                     .Where(t => !relatedTables.Contains(t)) // Tables that are not already related
                     .SelectMany(t => t.Columns)     // All columns of those tables
@@ -71,6 +71,47 @@ namespace TabularEditor.UI.Actions
 
                     lastTable = c.column.Table;
                     lastMatch = c.nameMatch;
+                }
+
+                // Add tables that already have a relationship:
+                if (relatedTables.Count > 0)
+                {
+                    candidateCols = col.Model.Tables
+                       .Where(t => t != col.Table) // All tables except parent table of current column
+                       .Where(t => relatedTables.Contains(t)) // Tables that are already related
+                       .SelectMany(t => t.Columns)     // All columns of those tables
+                       .Where(c => c.DataType == col.DataType && !col.Model.Relationships.Any(r => (r.FromColumn == col && r.ToColumn == c) || (r.FromColumn == c && r.ToColumn == col))) // Matching data types
+                       .Select(c =>
+                       {
+                           var n = c.Name.Replace(" ", "");
+                           return new
+                           {
+                               column = c,
+                               nameMatch = n.EndsWith(compactName) || compactName.EndsWith(n)
+                           };
+                       }
+                       )
+                       .ToList();      // Materialise
+
+                    if (candidateCols.Count > 0)
+                    {
+                        result.Add("---", null);
+
+                        lastTable = null;
+                        lastMatch = false;
+                        // Populate the dictionary ordered by table names, columns with partial name match, column name:
+                        foreach (var c in candidateCols.OrderBy(c => c.column.Table.Name).ThenBy(c => !c.nameMatch).ThenBy(c => c.column.Name))
+                        {
+                            // This adds a seperator between the columns that have a partial name match:
+                            if (lastTable == c.column.Table && lastMatch && !c.nameMatch)
+                                result.Add(c.column.Table.Name.ConcatPath("---"), null);
+
+                            result.Add(c.column.Table.Name.ConcatPath(c.column.Name), c.column);
+
+                            lastTable = c.column.Table;
+                            lastMatch = c.nameMatch;
+                        }
+                    }
                 }
 
                 return result;
