833e4474fbda7a088d726160144e6336c5fcfc7b
Daniel Otykier <do@kapacity.dk>
2021-03-19 14:46:50 +0100

    Bumped version numbers and added new certificate

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.14.*")]
-[assembly: AssemblyInformationalVersion("2.14.2")]
+[assembly: AssemblyVersion("2.15.*")]
+[assembly: AssemblyInformationalVersion("2.15.0")]
