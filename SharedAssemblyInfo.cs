21e60fac756624bdf634e6a2bf232a356e605328
Daniel Otykier <do@kapacity.dk>
2021-04-21 22:21:17 +0200

    Updated version numbers

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.15.*")]
-[assembly: AssemblyInformationalVersion("2.15.3")]
+[assembly: AssemblyVersion("2.16.*")]
+[assembly: AssemblyInformationalVersion("2.16.0")]
