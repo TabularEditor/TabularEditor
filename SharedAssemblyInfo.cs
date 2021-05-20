100cbfd335559dd00014c26b199f0488d358368c
Alberto Spelta <alberto.spelta@live.com>
2021-05-20 16:10:37 +0200

    Installer - atomatically set product version using a binding variable to AssemblyFileVersion

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyVersion("2.16.*")]
-[assembly: AssemblyInformationalVersion("2.16.1")]
+[assembly: AssemblyFileVersion("2.16.2")] // Attribute used by WiX Toolset to bind a variable and set the MSI installer to the same version of this assembly
+[assembly: AssemblyInformationalVersion("2.16.2")]
