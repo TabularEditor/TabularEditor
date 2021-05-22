3d15db018664fcb9d361e48946f7a328bbe2bb73
Alberto Spelta <alberto.spelta@live.com>
2021-05-22 10:07:13 +0200

    Installer - update message string

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyVersion("2.16.*")]
-[assembly: AssemblyFileVersion("2.16.2")] // Attribute used by WiX Toolset to bind a variable and set the MSI installer to the same version of this assembly
+[assembly: AssemblyFileVersion("2.16.2")] // AssemblyFileVersion is used by WiX Toolset to bind a variable and set the MSI installer to the same version of this assembly. If not present, AssemblyVersion is used.
 [assembly: AssemblyInformationalVersion("2.16.2")]
