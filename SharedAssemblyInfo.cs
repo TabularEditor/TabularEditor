3b12a022753f3a978850d821bdf712e97ead9c66
Daniel Otykier <daniel@tabulareditor.com>
2023-04-12 13:52:23 +0200

    Updated version number to 2.18.0

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.17.*")]
-[assembly: AssemblyInformationalVersion("2.17.3")]
+[assembly: AssemblyVersion("2.18.*")]
+[assembly: AssemblyInformationalVersion("2.18.0")]
