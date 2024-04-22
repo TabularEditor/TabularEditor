92cdb89100631619951f8283a6c25f8aaadab412
Daniel Otykier <daniel@tabulareditor.com>
2024-04-22 12:03:40 +0200

    Use built-in TMDL serializer to remove metadata order refs

---
+++ TOMWrapper/MetadataSerializationOptionsBuilderExtension.cs
@@ -0,0 +1,23 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+using Microsoft.AnalysisServices.Tabular.Extensions;
+
+namespace Microsoft.AnalysisServices.Tabular.Serialization
+{
+    public static class MetadataSerializationOptionsBuilderExtension
+    {
+        public static MetadataSerializationOptionsBuilder WithRestrictedInformation(this MetadataSerializationOptionsBuilder builder, bool includeRestrictedInformation)
+        {
+            return includeRestrictedInformation
+                ? builder.WithRestrictedInformation()
+                : builder.WithoutRestrictedInformation();
+        }
+
+
+        public static MetadataSerializationOptionsBuilder WithMetadataOrderHints(this MetadataSerializationOptionsBuilder builder, bool includeMetadataOrderHints)
+        {
+            return includeMetadataOrderHints
+                ? builder.WithMetadataOrderHints()
+                : builder.WithoutMetadataOrderHints();
+        }
+    }
+}
