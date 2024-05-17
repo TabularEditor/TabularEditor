1e3fee226473c2952e54aae64edc95a6d229e042
Mathias Thierbach <mt-dev@outlook.com>
2024-05-17 09:50:02 +0200

    BPA: Removed UI dependencies

---
+++ TabularEditor/BestPracticeAnalyzer/IRuleDefinition.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace TabularEditor.BestPracticeAnalyzer
+{
+    public interface IRuleDefinition
+    {
+        string Name { get; }
+        IEnumerable<BestPracticeRule> Rules { get; }
+        bool Internal { get; }
+    }
+}
